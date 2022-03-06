2cd6689f275f292b333501a8d3fcbbcff59a4104
Arda Atakan Uçan <ardaatakanucan@gmail.com>
2022-03-06 17:06:05 +0300

    lex, scanner and program files

---
+++ myscanner.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+extern int yylex();	// returns our next token	1,2,3... that corresponds to INTEGER, IDENTIFIER, etc.
+extern int yylineno;	// current line number that our scanner is in
+extern char* yytext;	// is the actual lexeme		54543,a...
+
+
+
+int main(void) {
+	int token = yylex();	// got next token
+	while(token) {
+		printf("%d\n", token);	// prints the corresponding int value of the token
+		token = yylex();	// next token 
+	}
+	return 0;
+}
